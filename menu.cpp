f47dc96615955e8af572790cd103297c6f6bf757
rizsatrio <satrios.playgame@gmail.com>
2025-05-07 11:51:58 +0800

    lets go

--- menu.cpp
+++ menu.cpp
@@ -2,14 +2,19 @@
 #include <conio.h>
 using namespace std;
 
+void info(){
+system("cls");
+cout<< "Indonesia hebat, Poliban Juara";
+getch();
 
+}
 void dMenu(){
 system("cls");
 cout<<"Aplikasi Tampilan Menu"<<"\n";       
 cout<<"1. Menu Pertama"<<"\n";            
 cout<<"2. Menu Kedua"<<"\n";            
 cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
+cout<<"4. Informasi"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
@@ -43,10 +48,10 @@ do
     /* code */
     break;  
    case '4':
-    mPertama("ke- empat");
+    info();
     /* code */
     break;  
-  case '5':
+    case '5':
     /* code */
     break;
   
